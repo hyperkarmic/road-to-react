f9151f6b633f088c239f9f486d31c085eeb57c1b
hyperkarmic <randy_pan@yahoo.com>
2022-04-26 15:24:35 +0100

    keys are necessary because index values can change and that makes them an unstable means of labelleing array elements

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import logo from './logo.svg'
 import './App.css'
 
-function getTitle(title) {
-  return title
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]
 
 function App() {
   return (
     <div>
-      <h1>{getTitle('the end is the end of the beginning')}</h1>
+      <h1>my Hacker Stories</h1>
+
       <label htmlFor='search'>Search: </label>
       <input id='search' type='text' />
+      <hr />
+      <ul>
+        {list.map(function (item) {
+          return <li key={item.objectID}>{item.title}</li>
+        })}
+      </ul>
     </div>
   )
 }
