c112c1f9a516ad735a1ca8d90c8a1a6af7baeffb
hyperkarmic <randy_pan@yahoo.com>
2022-04-28 23:36:23 +0100

    first component created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css'
 
 const list = [
@@ -28,21 +27,27 @@ function App() {
       <label htmlFor='search'>Search: </label>
       <input id='search' type='text' />
       <hr />
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href='{item.url}'>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          )
-        })}
-      </ul>
+      <List />
     </div>
+  )
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href='{item.url}'>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        )
+      })}
+    </ul>
   )
 }
 
